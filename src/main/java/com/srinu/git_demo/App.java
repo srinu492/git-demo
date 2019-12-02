f5246434d559c55ec18512040629fbfb3d87e8aa
srinu <pavulurisrinivasulu@gmail.com>
2019-12-02 18:59:12 +0530

    added git-demo project to local repo

---
+++ src/main/java/com/srinu/git_demo/App.java
@@ -0,0 +1,15 @@
+package com.srinu.git_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public void addData(String data) {
+		System.out.println("data---" + data);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
