27d79db6d905b8bab50d861b74f701537e279a5c
srinu <pavulurisrinivasulu@gmail.com>
2019-12-02 20:07:43 +0530

    added if condition to check the data

--- src/main/java/com/srinu/git_demo/App.java
+++ src/main/java/com/srinu/git_demo/App.java
@@ -7,6 +7,9 @@ package com.srinu.git_demo;
 public class App {
 	public void addData(String data) {
 		System.out.println("data---" + data);
+		if (data.equals("srinu")) {
+			System.out.println("welcome...");
+		}
 	}
 
 	public static void main(String[] args) {
